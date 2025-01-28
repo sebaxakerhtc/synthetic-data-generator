dbadb5a1268118a723ae7b03df3866d114362aa6
sdiazlor <sdiazlor@gmail.com>
2025-01-28 15:11:51 +0100

    improve comments

--- examples/hf-serverless-deployment.py
+++ examples/hf-serverless-deployment.py
@@ -9,8 +9,8 @@ import os
 from synthetic_dataset_generator import launch
 
 os.environ["HF_TOKEN"] = "hf_..."  # push the data to huggingface
-os.environ["MODEL"] = "meta-llama/Llama-3.1-8B-Instruct"  # use instruct model
-os.environ["MODEL_COMPLETION"] = "meta-llama/Llama-3.1-70B-Instruct"  # use model for completions
+os.environ["MODEL"] = "meta-llama/Llama-3.1-8B-Instruct"  # use model for instruction generation
+os.environ["MODEL_COMPLETION"] = "meta-llama/Llama-3.1-70B-Instruct"  # use a different model for completions
 os.environ["MAGPIE_PRE_QUERY_TEMPLATE"] = "llama3"  # use the template for the model
 
 launch()
